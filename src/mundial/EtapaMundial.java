e0342beafeadf4eda3a20f0a49c8351f870f51b5
debby <deborah.monla@davinci.edu.ar>
2025-07-01 18:29:37 +0200

    EtapaMundial casi lista

--- src/mundial/EtapaMundial.java
+++ src/mundial/EtapaMundial.java
@@ -1,4 +1,52 @@
+// src/mundial/EtapaMundial.java
 package mundial;
 
-public class EtapaMundial {
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+
+
+public abstract class EtapaMundial {
+    protected String descripcionEtapa;
+    protected List<Partido> partidos;
+
+    public EtapaMundial(String descripcionEtapa) {
+        this.descripcionEtapa = descripcionEtapa;
+        this.partidos = new ArrayList<>();
+    }
+
+    public String getDescripcionEtapa() {
+        return descripcionEtapa;
+    }
+
+    public void addPartido(Partido partido) {
+        partidos.add(partido);
+    }
+
+    public List<Partido> getPartidos() {
+        return Collections.unmodifiableList(partidos);
+    }
+
+    /**
+     * Resumen de resultados de partidos
+     */
+    public List<String> getResumenResultados() {
+        return partidos.stream()
+                .map(p -> String.format("%tF – %s %d:%d %s",
+                        p.getFecha(),
+                        p.getLocal().getNombre(),
+                        p.getResultado().getGolesLocal(),
+                        p.getResultado().getGolesVisitante(),
+                        p.getVisitante().getNombre()))
+                .toList();
+    }
+
+    public abstract List<Equipo> getEquiposQueAvanzan();
+
+    @Override
+    public String toString() {
+        return "EtapaMundial{" +
+                "descripcionEtapa='" + descripcionEtapa + '\'' +
+                ", partidos=" + partidos.size() + '}';
+    }
 }
