a44b045e603a568ab5a69d7d838c094b0a82ee31
Agustina michienzi <agustina.michienzi@davinci.edu.ar>
2025-07-01 18:56:33 -0300

    add two activitys and design the connection between then

--- src/mundial/Equipo.java
+++ src/mundial/Equipo.java
@@ -1,4 +1,15 @@
 package mundial;
 
 public class Equipo {
+    private final String nombre;
+    private int puntos;
+
+    public Equipo (String nombre){
+        this.nombre = nombre;
+        this.puntos = 0;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
 }
