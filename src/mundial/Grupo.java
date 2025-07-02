fc28caac8c1c0207f73599c0cc7b19cd6c495a40
Agustina michienzi <agustina.michienzi@davinci.edu.ar>
2025-07-02 11:26:11 -0300

    add two activitys and design the connection between then

--- src/mundial/Grupo.java
+++ src/mundial/Grupo.java
@@ -1,4 +1,21 @@
 package mundial;
 
+import java.util.*;
+import java.util.stream.*;
+
 public class Grupo {
+    private final String nombre;
+    private final List<Partido> partidos = new ArrayList<>();
+
+    public Grupo(String nombre){
+        this.nombre = nombre;
+    }
+
+    public void agregarPartido(Partido partido){
+        partidos.add(partido);
+    }
+
+
+
+
 }
