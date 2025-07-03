61be60bf8673c423f21b84d09f7fcfd92e87880e
Agustina michienzi <agustina.michienzi@davinci.edu.ar>
2025-07-03 12:02:59 -0300

    add two activitys and design the connection between then

--- src/mundial/Grupo.java
+++ src/mundial/Grupo.java
@@ -15,7 +15,26 @@ public class Grupo {
         partidos.add(partido);
     }
 
+    /**
+     * Devuelve los dos equipos con más puntos
+     * (suponiendo que no hay empates en puntos)
+     */
 
+    public List<Equipo> obtenerClasificados(){
+        return partidos.stream()
+                .flatMap(p -> Stream.of(p.getEquipoA(), p.getEquipoB()))
+                .distinct()
+                .sorted(Comparator.comparingInt(Equipo::getPuntos).reversed())
+                .limit(2)
+                .collect(Collectors.toList());
+    }
 
-
+    public void mostrarTabla() {
+        System.out.println("=== Grupo" + nombre + "===");
+        partidos.stream()
+                .flatMap(p -> Stream.of(p.getEquipoA(), p.getEquipoB()))
+                .distinct()
+                .sorted(Comparator.comparingInt(Equipo::getPuntos).reversed())
+                .forEach(e -> System.out.println(e));
+    }
 }
