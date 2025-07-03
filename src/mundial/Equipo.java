b324ad1d44ab64bdcaa0272f242cca4bfbaa4e8c
Agustina michienzi <agustina.michienzi@davinci.edu.ar>
2025-07-03 12:36:34 -0300

    add two activitys and design the connection between then

--- src/mundial/Equipo.java
+++ src/mundial/Equipo.java
@@ -1,28 +1,48 @@
 package mundial;
 
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Objects;
+
+/**
+ * Modelo de un equipo participante.
+ */
 public class Equipo {
-    private final String nombre;
-    private int puntos;
+    private String nombre;
+    private List<Partido> partidosJugados;
 
-    public Equipo (String nombre){
+    public Equipo(String nombre) {
         this.nombre = nombre;
-        this.puntos = 0;
+        this.partidosJugados = new ArrayList<>();
     }
 
-    public String getNombre(){
-        return nombre;
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+
+    public void addPartido(Partido p) {
+        partidosJugados.add(p);
     }
 
-    public int getPuntos(){
-        return puntos;
+    public List<Partido> getPartidosJugados() {
+        return Collections.unmodifiableList(partidosJugados);
     }
 
-    public void sumarPuntos(int pts){
-        this.puntos += pts;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Equipo)) return false;
+        Equipo eq = (Equipo) o;
+        return Objects.equals(nombre, eq.nombre);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nombre);
     }
 
     @Override
     public String toString() {
-        return nombre + "("+ puntos +" pts)";
+        return "Equipo{" + "nombre='" + nombre + '\'' + '}';
     }
 }
